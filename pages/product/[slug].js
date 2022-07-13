b633e3d082b5f5205ff16c1f6dbdf2f8291973f3
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2022-07-13 06:58:16 +0530

    13-07-2022
    cart handling with store procedure

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,31 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
 import Image from 'next/image';
+import { Store } from '../../utils/Store';
 
 function ProductScreen() {
+    const { state, dispatch } = useContext(Store);
+
     const {query} = useRouter();
     const {slug} = query;
     const product = data.products.find((x) => x.slug === slug);
     if(!product){
         return <div>Product Item not found</div>
     }
+
+    const addToCartHandler = () => {
+      const existingItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+      const quantity = existingItem ? existingItem.quantity + 1 : 1;
+      if (product.countInStock < quantity) {
+        alert('sorry. Product is out of stock');
+        return;
+      }
+      dispatch({ type: 'CART_ADD_ITEM', payload: {...product, quantity} });
+    }
+
   return (
     <Layout title={product.name}>
         <Link href="/">Home</Link><a>{'>'} {product.name}</a>
@@ -46,7 +60,7 @@ function ProductScreen() {
                 <div>Status</div>
                 <div>{product.countInStock > 0 ? 'In Stock' : 'Unavailable'}</div>
               </div>
-              <button className='primary-button w-full'>Add to cart</button>
+              <button className='primary-button w-full' onClick={addToCartHandler}>Add to cart</button>
             </div>
           </div>
         </div>
--- utils/Store.js
+++ utils/Store.js
@@ -15,7 +15,20 @@ function reducer(state, action) {
             const existItem = state.cart.cartItems.find(
                 (item) => item.slug === newItem.slug
             );
-            const cartItems = existItem? state.cart.cartItems.map((item) => item.name === existItem.name ? newItem : item): 
+            const cartItems = existItem? state.cart.cartItems.map((item) => 
+            item.name === existItem.name ? newItem : item
+            ):
+            [...state.cart.cartItems, newItem ];
+            return {...state, cart: {...state.cart, cartItems} }
         }
+        default:
+            return state;
     }
+}
+
+
+export function StoreProvider({ children }) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const value = { state, dispatch };
+    return <Store.Provider value={value}>{children} </Store.Provider>
 }
